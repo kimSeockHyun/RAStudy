2560d0cb3d7c302f364b1f7b68757d56d546c117
KimSeokHyun <devksh0624@kakao.com>
2020-07-26 14:13:20 +0900

    application 잘못된 경로로 인한 수정

--- src/main/java/com/RaStudyApplication.java
+++ src/main/java/com/RaStudyApplication.java
@@ -2,6 +2,8 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class RaStudyApplication {
@@ -9,5 +11,15 @@ public class RaStudyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RaStudyApplication.class, args);
 	}
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+ 
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+ 
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
 
 }
